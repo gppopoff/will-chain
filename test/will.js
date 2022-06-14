c1f9a678fd6a309dbc22b9b674047039a71a6ee4
Georgi Popov <37405027+gppopoff@users.noreply.github.com>
2022-06-14 23:38:21 +0300

    BACHKAT TESTOVE VSICHKO

--- test/will.js
+++ test/will.js
@@ -16,14 +16,21 @@ contract("DeathCertificate", function(accounts) {
                 console.log("Wills balance: " + await web3.eth.getBalance(instance.address))
 
                 let accounts = await web3.eth.getAccounts() 
-                console.log("Accounts with ethers: \n"+ accounts)
-                console.log(await web3.eth.getBalance(accounts[0]))
+                // console.log("Accounts with ethers: \n"+ accounts)
+                console.log("Address[0] balance: "+ await web3.eth.getBalance(accounts[0]));
+                console.log("Address[1] balance: "+ await web3.eth.getBalance(accounts[1]));
                 
                 await instance.register('1234', {from: accounts[0]});
                 assert.equal(await instance.amIReggistered('1234'), true);
                 console.log('Is 1234 registered: ' + await instance.amIReggistered('1234'))
                 await instance.createWill('1234', [{to: accounts[1], amount:3}], {from: accounts[0], value: 3000000000000000000} )
                 console.log(await instance.getMyWill({from: accounts[0]}));
+
+                console.log("DC address: " + ii.address);
+                console.log("DC balance: " + await web3.eth.getBalance(ii.address))
+                console.log("Wills address: " + instance.address)
+                console.log("Wills balance: " + await web3.eth.getBalance(instance.address))
+
                 // await instance.setDeathSertificateAddr(ii.address);
                 // await instance.setDeathSertificateAddr(accounts[0]);
                 // await instance.executeWills('1234');
@@ -31,7 +38,9 @@ contract("DeathCertificate", function(accounts) {
                 await ii.announceDeath1({announcer: {NIN: '4321'}, dead: {NIN: '1234'}, doctor: {NIN: '0000'}})
 
                 
+                console.log(await instance.getMyWill({from: accounts[0]}));
                 console.log("Address[0] balance: "+ await web3.eth.getBalance(accounts[0]));
+                console.log("Address[1] balance: "+ await web3.eth.getBalance(accounts[1]));
                 console.log("DC address: " + ii.address);
                 console.log("DC balance: " + await web3.eth.getBalance(ii.address))
                 console.log("Wills address: " + instance.address)
@@ -40,37 +49,39 @@ contract("DeathCertificate", function(accounts) {
         })
     })
 
-    // it("test deleteWill", function() {
-    //     return EtherWill.deployed().then(function(instance){
-    //         return DeathCertificate.deployed(instance.address).then( async function(ii){
-    //             console.log("DC address: " + ii.address);
-    //             console.log("DC balance: " + await web3.eth.getBalance(ii.address))
-    //             console.log("Wills address: " + instance.address)
-    //             console.log("Wills balance: " + await web3.eth.getBalance(instance.address))
-
-    //             let accounts = await web3.eth.getAccounts() 
-    //             console.log("Accounts with ethers: \n"+ accounts)
-    //             console.log(await web3.eth.getBalance(accounts[0]))
-                
-    //             await instance.register('1234', {from: accounts[0]});
-    //             console.log('Address of NIN: 1234: ' + await instance.accounts('1234'))
-    //             await instance.createWill('1234', [{to: accounts[1], amount:2}], {from: accounts[0], value: 3000000000000000000} )
-
-    //             await instance.createWill('1234', [{to: accounts[2], amount:1}], {from: accounts[0], value: 1000000000000000000} )
-    //             await instance.createWill('1234', [{to: accounts[4], amount:2}], {from: accounts[0], value: 2000000000000000000} )
-    //             await instance.createWill('1234', [{to: accounts[2], amount:3}], {from: accounts[0], value: 3000000000000000000} )
-    //             console.log(await instance.getMyWill({from: accounts[0]}));
-
-    //             await instance.deleteWillTo('1234', accounts[2], {from: accounts[0]});
-    //             console.log(await instance.getMyWill({from: accounts[0]}));
-
-
-    //             console.log("Address[0] balance: "+ await web3.eth.getBalance(accounts[0]));
-    //             console.log("DC address: " + ii.address);
-    //             console.log("DC balance: " + await web3.eth.getBalance(ii.address))
-    //             console.log("Wills address: " + instance.address)
-    //             console.log("Wills balance: " + await web3.eth.getBalance(instance.address))
-    //         })
-    //     })
-    // })
+   
 });
+
+contract("Ebi se", function() {
+    it("MainFlow", function() {
+        return EtherWill.deployed().then(async function(instance){
+                console.log("-------------------------------------------------------------------------")
+                console.log("Wills address: " + instance.address)
+                console.log("Wills balance: " + await web3.eth.getBalance(instance.address))
+
+                let accounts = await web3.eth.getAccounts() 
+                // console.log("Accounts with ethers: \n"+ accounts)
+                console.log("Address[0] balance: "+ await web3.eth.getBalance(accounts[4]));
+                
+                await instance.register('2222', {from: accounts[4]});
+                console.log('Is 2222 registered: ' + await instance.amIReggistered('2222'))
+                // await instance.createWill('2222', [{to: accounts[1], amount:1}], {from: accounts[4], value: 1000000000000000000} )
+                await instance.createWill('2222', [{to: accounts[2], amount:1}], {from: accounts[4], value: 1000000000000000000} )
+                // await instance.createWill('2222', [{to: accounts[3], amount:1}], {from: accounts[4], value: 1000000000000000000} )
+                await instance.createWill('2222', [{to: accounts[2], amount:1}], {from: accounts[4], value: 1000000000000000000} )
+                console.log(await instance.getMyWill({from: accounts[4]}));
+
+                console.log("Wills address: " + instance.address)
+                console.log("Wills balance: " + await web3.eth.getBalance(instance.address))
+
+                await instance.deleteWillTo('2222', accounts[2], {from: accounts[4]} )
+                
+                console.log(await instance.getMyWill({from: accounts[4]}));
+                console.log("Address[0] balance: "+ await web3.eth.getBalance(accounts[4]));
+                console.log("Wills address: " + instance.address)
+                console.log("Wills balance: " + await web3.eth.getBalance(instance.address))
+        })
+    })
+})
+
+
